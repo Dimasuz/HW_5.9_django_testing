0984df1f17701e41bfb15b8d6fea5029445fdaa6
Dimasuz <dimaskus@gmail.com>
2023-01-16 21:03:50 +0300

    7

--- tests/students/test_courses_api.py
+++ tests/students/test_courses_api.py
@@ -238,4 +238,4 @@ def test_get_firstpage(client):
 
     # Assert
     assert response.status_code == 200
-    assert response.content == b'Hello, from app - Students!'
+    assert response.content == b'Hello, from app - Students1!'
